0c1b581a46c5655b111ef7027ce2076bc4fe6162
kevin-convy-ats <kevin.convy@atsid.com>
2013-03-04 12:59:41 -0800

    Add pattern validation to test model.

--- src/test/data/SimpleTestModelSchema.js
+++ src/test/data/SimpleTestModelSchema.js
@@ -1,24 +1,19 @@
-define([
-], function (
-) {
-    
-    return {
-        "id": "TestData/SimpleTestModelSchema",
-        "description": "A simple model for testing",
-        "$schema": "http://json-schema.org/draft-03/schema",
-        "type": "object",
-        "properties": {
-            "modelNumber": {
-                "type": "string",
-                "maxLength": 4,
-                "description": "The number of the model.",
-                "required": true
-            },
-            "optionalprop": {
-                "type": "string",
-                "description": "an optional property.",
-                "optional": true
-            }
+define({
+    "id": "TestData/SimpleTestModelSchema",
+    "description": "A simple model for testing",
+    "$schema": "http://json-schema.org/draft-03/schema",
+    "type": "object",
+    "properties": {
+        "modelNumber": {
+            "type": "string",
+            "maxLength": 4,
+            "description": "The number of the model.",
+            "pattern": "[0-9]",
+            "required": true
+        },
+        "optionalprop": {
+            "type": "string",
+            "description": "an optional property."
         }
-    };
+    }
 });
