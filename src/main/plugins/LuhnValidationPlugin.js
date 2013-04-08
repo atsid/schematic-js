a89c6c5842d93ed85dcaf5f538436606d8d7bbad
chileric <chileric@gmail.com>
2013-04-08 15:14:05 -0700

    Adding Luhn validation plugin and unit test.

---
+++ src/main/plugins/LuhnValidationPlugin.js
@@ -0,0 +1,38 @@
+/**
+ * @class schematic-js/LuhnValidationPlugin
+ * Plugin to validate a number using the Luhn algorithm.
+ */
+define([
+    "../util"
+], function (
+    util
+    ) {
+    var module = function (config) {
+        util.mixin(this, config);
+
+        this.validate = function (property, instance, newValue, schema) {
+            var ret = [], value = newValue || instance[property],
+                sum = 0,
+                numdigits = value.length,
+                parity = numdigits % 2,
+                i,
+                digit;
+
+            if (value) {
+                for(i = 0; i < numdigits; i += 1) {
+                    digit = parseInt(value.charAt(i));
+                    if(i % 2 == parity) digit *= 2;
+                    if(digit > 9) {
+                        digit -= 9;
+                    }
+                    sum += digit;
+                }
+                if ((sum % 10) != 0) {
+                    ret.push("Property " + property + " is invalid");
+                }
+            }
+            return ret.length ? ret : undefined;
+        }
+    };
+    return module;
+});
