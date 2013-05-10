bffcf7014387b2d7bd37f430dc5e151aedabf2d1
ssmith7 <ssmith7@UL13216.uspto.gov>
2013-05-10 10:39:21 -0400

    Added ConditionallyRequiredPlugin and util.indexOf wrapper for array.indexOf

---
+++ src/main/plugins/ConditionallyRequiredValidationPlugin.js
@@ -0,0 +1,40 @@
+/**
+ * @class schematic-js/ConditionallyRequiredValidationPlugin
+ * Plugin to selectively validate properties against a json schema
+ * based on the values of another property in the schema.
+ * {
+ *    requiredMessage: {code:123, message: "Field required");
+ * }
+ */
+define([
+    "../util"
+], function (
+    util
+) {
+    var findProperty = function (schema, prop) {
+            var ret = schema && schema.properties && schema.properties[prop];
+            if (!ret && schema["extends"]) {
+                ret = findProperty(schema["extends"], prop);
+            }
+            return ret;
+        },
+        module = function (config) {
+            this.requiredMessage = {code: 0, message: "Field is required."};
+            util.mixin(this, config);
+
+            this.validate = function (property, instance, newValue, schema) {
+                var ret = [],
+                    value = newValue === undefined ? instance[property] : newValue,
+                    schemaProp = findProperty(schema, property),
+                    reqWhenValue = instance[this.requiredWhen.property],
+                    reqWhenArray = this.requiredWhen.values || [];
+                // required
+
+                if (util.indexOf(reqWhenValue, reqWhenArray) > -1 && schemaProp) {
+                    ret.push(this.requiredMessage);
+                }
+                return ret.length ? ret : undefined;
+            };
+        };
+    return module;
+});
