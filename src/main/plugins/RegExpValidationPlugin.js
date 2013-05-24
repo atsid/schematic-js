ddec5507d408263f0ac06889ba45de53f31fefee
ssmith7 <ssmith7@UL13216.uspto.gov>
2013-05-24 08:46:44 -0400

    Added optional configuration for conditional matching (ie, only match when another value is one of 1 or more values).

--- src/main/plugins/RegExpValidationPlugin.js
+++ src/main/plugins/RegExpValidationPlugin.js
@@ -15,9 +15,14 @@ define([
         this.validate = function (property, instance, newValue, schema) {
             var ret = [], 
                 value = newValue === undefined ? instance[property] : newValue,
-                regexp = new RegExp(this.pattern || ".*");
-            if (value !== "" && !regexp.test(value)) {
-                ret.push(that.message);
+                regexp = new RegExp(this.pattern || ".*"),
+                matchWhenValue = this.requiredWhen ? instance[this.requiredWhen.property] : '',
+                matchWhenArray = this.requiredWhen ? this.requiredWhen.values : [''];
+
+            if (util.indexOf(matchWhenValue, matchWhenArray) > -1) {                
+                if (value !== "" && !regexp.test(value)) {
+                    ret.push(that.message);
+                }
             }
             return ret.length ? ret : undefined;
         };
