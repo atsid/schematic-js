078ab950627ee540ca55a7e65d89578b3506645b
ssmith7 <ssmith7@UL13216.uspto.gov>
2013-07-31 11:44:27 -0400

    Validates when two parameters match each other.

---
+++ src/main/plugins/MatchingPropertiesValidationPlugin.js
@@ -0,0 +1,42 @@
+/**
+ * @class schematic-js/ConditionallyRequiredValidationPlugin
+ * Plugin to selectively validate properties against a json schema
+ * based on the values of another property in the schema.
+ * {
+ *    requiredMessage: {code:123, message: "Field required");
+ * }
+ */
+define([
+    "../util"
+], function (
+    util
+) {
+    var findProperty = function (schema, prop) {
+            var ret = schema && schema.properties && schema.properties[prop];
+            if (!ret && schema["extends"]) {
+                ret = findProperty(schema["extends"], prop);
+            }
+            return ret;
+        },
+        module = function (config) {
+            var defaultMessage = {code: 0, message: "Fields are not equal"};
+            util.mixin(this, config);
+
+            this.validate = function (property, instance, newValue, schema) {
+                var ret = [],
+                    value = newValue === undefined ? instance[property] : newValue;
+                    
+                if (this.matchProperty && value !== instance[this.matchProperty]) {
+                      if (this.message && this.message.message) {
+                          ret.push(this.message);
+                      }
+                      else {
+                          ret.push(defaultMessage);          
+                      }
+                }
+
+                return ret.length ? ret : undefined;
+            };
+        };
+    return module;
+});
