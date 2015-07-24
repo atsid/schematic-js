3330316bc0577db3bf3e113ce40aca2265703522
Nathan Evans <github@talkswithnumbers.com>
2015-07-24 12:41:41 -0700

    #4 - moved ModelFactory.spec tests into TestModelFactory since they're both mocha now

--- test/TestModelFactory.js
+++ test/TestModelFactory.js
@@ -1,8 +1,12 @@
 define([
     "schematic/ModelFactory",
+    "schematic/plugins/SchemaValidationPlugin",
+    'TestData/BaseSchema',
     "TestData/SimpleTestModelSchema"
 ], function (
     ModelFactory,
+    SchemaValidationPlugin,
+    BaseSchema,
     SimpleTestModelSchema
 ) {
 
@@ -10,26 +14,79 @@ define([
 
     describe('ModelFactory', function () {
 
-        var factory;
+        describe('get models', function () {
 
-        beforeEach(function () {
-            factory = new ModelFactory({
-                resolver: function () {
-                    return SimpleTestModelSchema;
-                }
+            var factory;
+
+            beforeEach(function () {
+                factory = new ModelFactory({
+                    resolver: function () {
+                        return SimpleTestModelSchema;
+                    }
+                });
             });
-        });
 
-        it('get model by name', function () {
-            var model = factory.getModel("TestData/SimpleTestModelSchema");
-            assert.isDefined(model);
-        });
+            it('get model by name', function () {
+                var model = factory.getModel("TestData/SimpleTestModelSchema");
+                assert.isDefined(model);
+            });
 
-        it('get model by schema instance', function () {
-            var model = factory.getModel(SimpleTestModelSchema);
-            assert.isDefined(model);
-        });
+            it('get model by schema instance', function () {
+                var model = factory.getModel(SimpleTestModelSchema);
+                assert.isDefined(model);
+            });
 
+        });
+        
+        describe('validate models', function () {
+
+            var factory, model;
+
+            beforeEach(function () {
+                factory = new ModelFactory();
+                factory.addValidator(new SchemaValidationPlugin({
+                    propertyPattern: /.*/,
+                    modelPattern: /.*/
+                }));
+                model = factory.getModel(BaseSchema);
+            });
+
+            it("can return a model based on a schema.", function () {
+                model.set('modelNumber', "1234");
+                assert.equal("1234", model.modelNumber);
+            });
+
+            it("allows unvalidated set by default", function () {
+                model.set('modelNumber', "12345678");
+                assert.equal("12345678", model.modelNumber);
+            });
+
+            it("can be configured to disallow 'set' based on validation.", function () {
+                factory = new ModelFactory({
+                    validatedSet: true
+                });
+                factory.addValidator(new SchemaValidationPlugin({
+                    propertyPattern: /.*/,
+                    modelPattern: /.*/
+                }));
+                model = factory.getModel(BaseSchema);
+                model.set('modelNumber', "1234");
+                assert.equal("1234", model.modelNumber);
+                model.set('modelNumber', "12345678");
+                assert.equal("1234", model.modelNumber);
+            });
+
+            it("allows explicit validation of the entire model", function () {
+                model.set('modelNumber', "12345678");
+                assert.equal("12345678", model.modelNumber);
+                model.validate();
+                assert.isDefined(model.lastErrors.modelNumber);
+            });
+            
+        });
+        
     });
+
+
 
 });
